fcb86bec2416ebaff6c9ac46a5b288b91a193f6e
Dmitrii <veter.666@list.ru>
2018-08-20 19:15:36 +1000

    created class hand

---
+++ hand.rb
@@ -0,0 +1,40 @@
+class Hand
+  def initialize
+    @cards = []
+  end
+
+  def take_card(card_deck)
+    @cards << card_deck.take_card
+  end
+
+  def show_cards
+    cards
+  end
+
+  def amt_card
+    cards.length
+  end
+
+  def amt_points
+    total = sum_points
+    total = amt_points if total > 21 && change_points
+    total
+  end
+
+  protected
+
+  attr_reader :cards
+
+  def sum_points
+    total = 0
+    cards.each { |card| total += card[:points] }
+    total
+  end
+
+  def change_points
+    ace_eleven = cards.find { |card| card[:rang] == 'A' && card[:points] == 11 }
+    return false unless ace_eleven
+    ace_eleven[:points] = 1
+    true
+  end
+end
