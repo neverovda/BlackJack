fe5b8ca659b9ce3df209113f74584fbaf8226757
Dmitrii <veter.666@list.ru>
2018-08-20 11:45:49 +1000

    created card deck

---
+++ card_deck.rb
@@ -0,0 +1,22 @@
+class CardDeck
+  SUIT = %i[clubs diamonds hearts spades].freeze
+  RANG_WITH_POINTS = { '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6,
+                       '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'J' => 10,
+                       'Q' => 10, 'K' => 10, 'A' => 11 }.freeze
+
+  def initialize
+    @cards = []
+    RANG_WITH_POINTS.each do |rang, points|
+      SUIT.each { |suit| @cards << make_card(rang, suit, points) }
+    end
+    @cards.shuffle!
+  end
+
+  def make_card(rang, suit, points)
+    { rang: rang, suit: suit, points: points }
+  end
+
+  def take_card
+    @cards.pop
+  end
+end
